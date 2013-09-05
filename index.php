35c3de14e3bcf26caa7b3d3a38707d8e8afcbc02
Jan-Gerd Tenberge <janten@gmail.com>
2013-09-05 18:08:00 +0200

    Restructured some source code

--- index.php
+++ index.php
@@ -1,25 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>John Does photos</title>
-        <link rel="stylesheet" href="includes/styles/style.css" type="text/css">
-
-        <link rel="shortcut icon" href="/includes/images/favicon.ico"/>
-        <link rel="apple-touch-icon-precomposed" href="/includes/images/ios/icon.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/includes/images/ios/icon-72.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/includes/images/ios/icon@2x.png" />
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/includes/images/ios/icon-72@2x.png" />
-
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        
-		<!-- Uncomment and replace with your personal profile URLs if you want to  -->
-        <!-- <link rel="author" href="https://plus.google.com/123456789">          -->
-        <!-- <link rel="author" href="https://facebook.com/johndoe">               -->
+		<?php require_once("includes/html/head.htm"); ?>
     </head>
     <body>
         <div id="container">
             <?php require_once("posts.htm"); ?>
-			<?php require_once("footer.htm"); ?>
+			<?php require_once("includes/html/foot.htm"); ?>
         </div>
 		
 		<!-- This would be a good place to insert Google Analytics code -->
